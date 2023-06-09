f94a699168cdfc9c3495eb54fb96b19595d4afcc
LJH <junhao.liu.sLab@gmail.com>
2023-06-09 14:28:38 +0900

    输出结果-数据库

---
+++ src/main/java/com/ItemWriter_db/UserPreStatementSetter.java
@@ -0,0 +1,16 @@
+package com.ItemWriter_db;
+
+import org.springframework.batch.item.database.ItemPreparedStatementSetter;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+//设置占位符的参数
+public class UserPreStatementSetter implements ItemPreparedStatementSetter<User> {
+    @Override
+    public void setValues(User item, PreparedStatement ps)throws SQLException{
+        ps.setLong(1,item.getId());
+        ps.setString(2,item.getName());
+        ps.setInt(3,item.getAge());
+    }
+}
